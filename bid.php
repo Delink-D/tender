4032939359a906be1cc952433d7bd783eac30104
Delink-D <delinkdesigns@gmail.com>
2018-02-10 00:54:02 +0300

    included the tender details section

--- bid.php
+++ bid.php
@@ -7,6 +7,22 @@
     background: #000;
   }
 </style>
+<!-- tender intro -->
+<div class="container tender-body padding0">
+  <div id="tender-header" style="position: relative;">
+    <h3>Tender No. <strong>DLNK/T/04-02-2018</strong> Details</h3>
+  </div>
+  <div class="padding-10 group" style="position: relative;">
+    <div class="col-5 float-left">
+      <p class="c-text-bold tender-text-detail">Tender Category: Open to All</p>
+      <p class="c-text-bold tender-text-detail">Tender Notice Type: Infrastructure and construction</p>
+    </div>
+    <div class="col-5 float-right">
+      <p class="c-text-bold tender-text-detail">Tender Closing Day: <span class="text-danger">04-03-2018 12:00 PM</span></p>
+      <p class="c-text-bold tender-text-detail">Tender Security: 100000 Ksh</p>
+    </div>
+  </div>
+</div>
 
 <!-- body -->
 <div class="container body padding0">
