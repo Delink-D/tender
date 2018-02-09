d76adc10840e823deac38e3d19f07440abc45487
Delink-D <delinkdesigns@gmail.com>
2018-02-09 21:37:47 +0300

    navigation file

---
+++ include/navigation.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <!-- Navbar content -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Active Tenders</a>
+    </li> | 
+    <li class="nav-item">
+      <a class="nav-link" href="pasttenders.php">Past Tenders</a>
+    </li> | 
+    <li class="nav-item">
+      <a class="nav-link" href="expiredtenders.php">Expired Tenders</a>
+    </li> | 
+  </ul>
+
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0 btn-sm" type="submit">Search</button>
+  </form>
+</nav>
