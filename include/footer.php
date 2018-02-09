6fe9389fc4dafc44ebcd2e794abdda2de7f0b43f
Delink-D <delinkdesigns@gmail.com>
2018-02-09 21:38:59 +0300

    header and footer central files

---
+++ include/footer.php
@@ -0,0 +1,41 @@
+      </div>
+      <!-- footer -->
+      <div id="footer">
+        <div class="container">
+          <div class="col-5 float-left">
+            <div>Last update: <?php echo date('D d F Y'); ?></div>
+            <div>&copy; <?php echo date('Y'); ?> e-Tenders</div>
+          </div>
+
+          <div class="col-7 float-right text-right">
+            <!-- Navbar content -->
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li> | 
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+              </li> | 
+               <li class="nav-item">
+                <a class="nav-link" href="faq.php">FAQ</a>
+              </li> |
+               <li class="nav-item">
+                <a class="nav-link" href="terms.php">Terms of Use</a>
+              </li> | 
+               <li class="nav-item">
+                <a class="nav-link" href="help.php">Help</a>
+              </li> | 
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li> | 
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- sripts -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ include/head.php
@@ -0,0 +1,63 @@
+<?php 
+  error_reporting(0);
+  session_start();
+
+  if ($_SESSION['userIn']) {
+    $username = $_SESSION['username'];
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>E-Tender</title>
+
+    <!-- styles -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+  </head>
+  <body>
+    <div class="container-fluid padding0">
+      <!-- header -->
+      <div id="header">
+        <div class="container">
+          <div id="logo" class="col-7 float-left">
+            <a href="index.php">
+              <img src="https://e-tender.ie/_images/design/logo.png" alt="E-Tender">
+            </a>
+          </div>
+
+          <div class="col-5 float-right">
+            <div class="text-right">
+              <?php if ($_SESSION['userIn']) {?>
+              <button type="button" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#signinModal">
+                SignUp
+              </button>
+              <button type="button" class="btn btn-outline-secondary btn-sm" data-toggle="modal" data-target="#loginModal">
+                SignIn
+              </button>
+
+              <?php } if (!$_SESSION['userIn']) {?>
+              <div class="dropdown">
+                <a class="btn dropdown-toggle" href="#" role="button" id="profile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Your User Name <i class="material-icons">account_circle</i>
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="profile">
+                  <a class="dropdown-item" href="#">Profile</a>
+                  <a class="dropdown-item" href="#">Active Bids</a>
+                  <a class="dropdown-item" href="#">Past Tenders</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Sign Out</a>
+                </div>
+              </div>
+              <?php } ?>
+            </div>
+
+            <!-- include modal file -->
+            <?php include 'include/modal.php'; ?>
+          </div>
+        </div>
+      </div>
+      <!-- body -->
+      <div class="container body padding0">
