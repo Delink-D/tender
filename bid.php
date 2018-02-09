6b7597be49bfc8a9d0792d2ea1ff2e815e2a5503
Delink-D <delinkdesigns@gmail.com>
2018-02-10 00:53:00 +0300

    added a bid file

---
+++ bid.php
@@ -0,0 +1,79 @@
+<?php  
+  include 'include/head.php';
+  include 'include/modal.php';
+?>
+<style>
+  body {
+    background: #000;
+  }
+</style>
+
+<!-- body -->
+<div class="container body padding0">
+  <table class="table table-sm table-hover table-striped table-dark">
+    <thead>
+      <tr>
+        <th colspan="7" class="text-center h4" style="background: #0B1726;">TOP BIDDERS ON THIS TENDER</th>
+      </tr>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Company</th>
+        <th scope="col">Bidding Comp</th>
+        <th scope="col">Bid By</th>
+        <th scope="col">Company Category</th>
+        <th scope="col">Bid Date</th>
+        <th scope="col">Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="bg-success">
+        <th scope="row">1</th>
+        <td>Delink Inc</td>
+        <td>DLNK/T/04-02-2018</td>
+        <td>UserName</td>
+        <td>Open to All</td>
+        <td>04-03-2018 12:00 PM</td>
+        <td>250000 Ksh</td>
+      </tr>
+      <tr class="bg-primary">
+        <th scope="row">1</th>
+        <td>Delink In</td>
+        <td>LDTD/T/04-05-2018</td>
+        <td>UserName</td>
+        <td>Open to All</td>
+        <td>04-03-2018 12:00 PM</td>
+        <td>270000 Ksh</td>
+      </tr>
+      <tr class="bg-warning">
+        <th scope="row">1</th>
+        <td>Delink Inc</td>
+        <td>DLNK/T/04-02-2018</td>
+        <td>UserName</td>
+        <td>Open to All</td>
+        <td>04-03-2018 12:00 PM</td>
+        <td>250000 Ksh</td>
+      </tr>
+      <tr>
+        <th scope="row">1</th>
+        <td>Delink In</td>
+        <td>LDTD/T/04-05-2018</td>
+        <td>UserName</td>
+        <td>Open to All</td>
+        <td>04-03-2018 12:00 PM</td>
+        <td>270000 Ksh</td>
+      </tr>
+      <tr>
+        <th scope="row">1</th>
+        <td>Delink Inc</td>
+        <td>DLNK/T/04-02-2018</td>
+        <td>UserName</td>
+        <td>Open to All</td>
+        <td>04-03-2018 12:00 PM</td>
+        <td>250000 Ksh</td>
+      </tr>
+    </tbody>
+  </table>
+
+<?php  
+  include 'include/footer.php';
+?>
