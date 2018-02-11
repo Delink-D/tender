f541c19483768f612c6bd612244fd8bb4d4ea160
Delink-D <delinkdesigns@gmail.com>
2018-02-11 08:53:10 +0300

    now connected to the database

---
+++ database/db.php
@@ -0,0 +1,15 @@
+<!-- connecting to the database -->
+<?php 
+  
+  // set values
+  $host = 'localhost';
+  $user = 'root';
+  $password = '';
+  $database = 'tenders';
+
+  $db = mysqli_connect($host, $user, $password, $database);
+
+  // check if connected
+  if (!$db) {
+    echo "<h2>DATABASE CONNECTION ERROR</h2>";
+  }
