4a876b8f1bf73ec824d17cb228e9670ca652f732
Delink-D <delinkdesigns@gmail.com>
2018-02-16 22:16:13 +0300

    from the user inputs query the database with username

--- functions/login.php
+++ functions/login.php
@@ -9,7 +9,16 @@ $pass = $_POST['password'];
 
 if ($_POST['login']) {
   // get user details
+  $sql = "SELECT * FROM users WHERE username = '$userId'";
+  $query = mysqli_query($db, $sql);
 
+  if (mysqli_num_rows($query) !== 1) {
+    // userId error
+    header("location: " . $server ."?msg=user");
+  }
+
+  $uRow = mysqli_fetch_assoc($query);
+  $user = $uRow;
 } else {
   header("location: " . $server ."?msg=required");
 }
