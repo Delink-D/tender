d2672c14baf2c6c48fd66890dc8a9344b9b03eff
Delink-D <delinkdesigns@gmail.com>
2018-02-17 23:07:37 +0300

    query and diasplay the companies on ui

--- admin/addcompany.php
+++ admin/addcompany.php
@@ -64,18 +64,64 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Delink Inc</td>
-        <td>DLNK/T/04-02-2018</td>
-        <td>Youth and Women</td>
-        <td>info@delink.com</td>
-        <td>04 July 2011</td>
-        <td>
-          <a href="#" class="btn btn-sm btn-primary">Edit</a>
-          <a href="#" class="btn btn-sm btn-danger">Delete</a>
-        </td>
-      </tr>
+      <?php
+        $c_sql = mysqli_query($db, "SELECT * FROM companies");
+        if (mysqli_num_rows($c_sql) < 1) {
+          echo "
+            <tr>
+              <th colspan='7' class='text-center'>THERE ARE NO COMPANIES YET!!</th>
+            </tr>
+          ";
+        }
+
+        $rowNo = 0;
+        while ($row = mysqli_fetch_assoc($c_sql)) {
+          $c_id = $row['_id'];
+          $c_name = $row['company_name'];
+          $c_reg = $row['company_reg'];
+          $c_cat = $row['company_cat'];
+          $c_email = $row['company_email'];
+          $c_date = date('M d Y', strtotime($row['dateadded']));
+          $rowNo ++;
+
+          switch ($c_cat) {
+            case 'y':
+              $category = "Youth";
+              break;
+            
+            case 'yw':
+              $category = "Youth & Women";
+              break;
+
+            case 'm':
+              $category = "Minorities";
+              break;
+
+            case 'g':
+              $category = "General";
+              break;
+
+            default:
+              $category = "N/A";
+              break;
+          }
+
+          echo "
+            <tr>
+              <td>$rowNo</td>
+              <td>$c_name</td>
+              <td>$c_reg</td>
+              <td>$category</td>
+              <td>$c_email</td>
+              <td>$c_date</td>
+              <td>
+                <a href='$c_id' class='badge badge-primary'>Edit</a>
+                <a href='$c_id' class='badge badge-danger'>Delete</a>
+              </td>
+            </tr>
+          ";
+        }
+      ?>
     </tbody>
   </table>  
 </div>
