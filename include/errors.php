514a77a9fd1970a6d2fe9eb8e5c65b76c5b1cc53
Delink-D <delinkdesigns@gmail.com>
2018-02-17 20:36:59 +0300

    included more errors on the error file

--- include/errors.php
+++ include/errors.php
@@ -1,4 +1,4 @@
-<div class="container text-center margin-top-10">
+<div class="container text-center margin-top-10 container-errors">
 <?php
   // get the error from the url
   $error = $_GET['msg'];
@@ -6,7 +6,7 @@
   if ($error === 'required') {
     echo "
       <div class='alert alert-danger' role='alert'>
-        User name and Password required!
+        All fields are required!!
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
           <span aria-hidden='true'>&times;</span>
         </button>
@@ -27,6 +27,46 @@
     echo "
       <div class='alert alert-success' role='alert'>
         Success login, welcome to <strong>E-Tender</strong>
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+    ";
+  }
+  if ($error === 'saved') {
+    echo "
+      <div class='alert alert-success' role='alert'>
+        Success saving a record to <strong>E-Tender</strong>
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+    ";
+  }
+  if ($error === 'error-login') {
+    echo "
+      <div class='alert alert-danger' role='alert'>
+        Error while trying to login to <strong>E-Tender</strong> try to login again
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+    ";
+  }
+  if ($error === 'error-fetching') {
+    echo "
+      <div class='alert alert-danger' role='alert'>
+        Error while trying to Fetch a record!! try again later!
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+    ";
+  }
+  if ($error === 'error-saving') {
+    echo "
+      <div class='alert alert-danger' role='alert'>
+        Error while saving a record!! try again later!
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
           <span aria-hidden='true'>&times;</span>
         </button>
