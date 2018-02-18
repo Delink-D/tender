6bcf874b053277b19b0ac567408a1006c236986c
Delink-D <delinkdesigns@gmail.com>
2018-02-18 22:07:13 +0300

    add tender page

---
+++ tenderadd.php
@@ -0,0 +1,13 @@
+<?php
+  include 'include/head.php';
+?>
+
+<!-- body -->
+<div class="container body padding0">
+  <?php // include navigation
+    include 'include/navigation.php';
+  ?>
+
+<?php
+  include 'include/footer.php';
+?>
