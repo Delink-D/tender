5a13ab2271ff8208e747994069b1d8e4270684f5
Delink-D <delinkdesigns@gmail.com>
2018-02-25 13:22:35 +0300

    check if tender alreadey exist in the db

--- functions/insert.tender.php
+++ functions/insert.tender.php
@@ -14,7 +14,13 @@ $t_desc = $_POST['t_desc'];
 $added_by = $_SESSION['_user']['username'];
 
 if ($_POST['insertTender']) {
-	
+	// check if tender exist
+	$cSql = "SELECT * FROM tenders WHERE tender_number = '$t_number'";
+	$cQuery = mysqli_query($db, $cSql);
+
+	if (mysqli_num_rows($cQuery) > 1) {
+		header('location: ' . $server . 'tenderadd.php?msg=exist');
+	}
 
 } else {
 	header('location: ' . $server . 'tenderadd.php');
