4e2106b79f44f9f8e67aa3bbd8593a86addb77d7
Delink-D <delinkdesigns@gmail.com>
2018-02-25 13:37:39 +0300

    implement the save to db

--- functions/insert.tender.php
+++ functions/insert.tender.php
@@ -1,5 +1,6 @@
 <?php
 // gather the user's input
+session_start();
 include '../env/db.php';
 include '../env/server.php';
 
@@ -20,6 +21,17 @@ if ($_POST['insertTender']) {
 
 	if (mysqli_num_rows($cQuery) > 1) {
 		header('location: ' . $server . 'tenderadd.php?msg=exist');
+	}
+
+	// save tender
+	$sql = "INSERT INTO tenders (tender_number, company_name, tender_cat, tender_type, tender_security, tender_closing, tender_desc, added_by)
+			VALUES('$t_number','$t_company','$t_category','$t_type','$t_security','$t_closing','$t_desc','$added_by')";
+	$query = mysqli_query($db, $sql);
+
+	if ($query) {
+		header('location: ' . $server . 'tenderadd.php?msg=saved');
+	} else {
+		header('location: ' . $server . 'tenderadd.php?msg=error-saving');
 	}
 
 } else {
