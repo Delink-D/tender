e2e37bfe4d3e83a6ddbcef4c68a56dcdac2375f0
Delink-D <delinkdesigns@gmail.com>
2018-09-02 02:37:17 +0300

    added functionality to add companies to the db

--- functions/insert.company.php
+++ functions/insert.company.php
@@ -1,7 +1,7 @@
 <?php
 // insert company
-include '../../env/db.php';
-include '../../env/server.php';
+session_start();
+include '../env/db.php';
 
 $regNo = $_POST['regNo'];
 $kra = $_POST['kra'];
@@ -13,23 +13,30 @@ $ccategory = $_POST['ccategory'];
 if ($_POST['saveCompany']) {
   // confirm if company exist
   $checkC = "SELECT * FROM companies WHERE company_reg = '$regNo' OR company_pin = '$kra' OR company_name = '$cname'";
-  $checkQ = mysqli_query($db, $checkC) or die(header('location: ' . $server . 'admin/?page=addcompany&msg=error-fetching'));
+  $checkQ = mysqli_query($db, $checkC) or die(header('location: ../company.php?page=addcompany&msg=error-fetching'));
 
   if (mysqli_num_rows($checkQ) > 0) {
     // return error if company exist
-    header('location: ' . $server . 'admin/?page=addcompany&msg=exist');
+    header('location: ../company.php?page=addcompany&msg=exist');
   }
   // insert company
   $sql = "INSERT INTO companies (company_reg, company_pin, company_name, comany_phone, company_email, company_cat)
           VALUES('$regNo','$kra','$cname','$cphone','$cemail','$ccategory')";
-  $query = mysqli_query($db, $sql) or die(header('location: ' . $server . 'admin/?page=addcompany&msg=error-saving'));
+  $query = mysqli_query($db, $sql) or die(header('location: ../company.php?page=addcompany&msg=error-saving'));
 
   if ($query) {
     // success of saving
-    header('location: ' . $server . 'admin/?page=addcompany&msg=saved');
+
+    // update user record to this company
+    $id = mysqli_insert_id($db);
+    $u_id = $_SESSION['_user']['_id'];
+    $update = "UPDATE users SET company = '$id' WHERE _id = '$u_id'";
+    mysqli_query($db, $update);
+
+    header('location: ../company.php?page=addcompany&msg=saved');
   }
 
 } else {
   // return with an error
-  header('location: ' . $server . 'admin/?page=addcompany&msg=required');
+  header('location: ../company.php?page=addcompany&msg=required');
 }
