5b85a0dc53268002ab691369f1ae47a16f1f3fc0
Delink-D <delinkdesigns@gmail.com>
2018-09-02 00:55:04 +0300

    added footer and header files to include to other pages

---
+++ admin/includes/head.php
@@ -0,0 +1,13 @@
+<?php   error_reporting(0); ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>E-Tender Management System | Admin</title>
+    <!-- styles -->
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="//cdn.materialdesignicons.com/2.1.19/css/materialdesignicons.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+  </head>
+  <body>
