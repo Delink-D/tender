f0dd1f32bd47bfe204f819d760580b7d9cc63fdc
Delink-D <delinkdesigns@gmail.com>
2018-09-02 00:21:38 +0300

    removed the company field from signup form added level

--- functions/signup.php
+++ functions/signup.php
@@ -9,7 +9,7 @@ $email = $_POST['email'];
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $natId = $_POST['natId'];
-$company = $_POST['company'];
+$level = $_POST['level'];
 $password = $_POST['password'];
 
 if ($_POST['signup']) {
@@ -17,55 +17,47 @@ if ($_POST['signup']) {
 
   // check if user exist
   $check = "SELECT * FROM users WHERE username = '$userName'";
-  $checkQ = mysqli_query($db, $check) or die(header('location: ' . $server . '?msg=error-fetching'));
+  $checkQ = mysqli_query($db, $check) or die(header('location: ../index.php?msg=error-fetching'));
 
   if (mysqli_num_rows($checkQ) > 0) {
-    header('location: ' . $server . '?msg=exist');
+    header('location: ../index.php?msg=exist');
     exit();
   } else {
     // insert user
-    $sql = "INSERT INTO users (firstname, lastname, username, email, nationalId, company, password)
-          VALUES('$firstName','$lastName','$userName','$email','$natId','$company','$password')";
-    $query = mysqli_query($db, $sql) or die(header('location: ' . $server . '?msg=error-saving'));
+    $sql = "INSERT INTO users (firstname, lastname, username, category, email, nationalId, password)
+          VALUES('$firstName','$lastName','$userName', '$level', '$email','$natId','$password')";
+    $query = mysqli_query($db, $sql) or die(header('location: ../index.php?msg=error-saving'));
 
     if ($query) {
       // get saved user
       $id = mysqli_insert_id($db);
       $getUser = "SELECT * FROM users WHERE _id = '$id'";
-      $qUser = mysqli_query($db, $getUser) or die(header('location: ' . $server . '?msg=error-fetching'));
+      $qUser = mysqli_query($db, $getUser) or die(header('location: ../index.php?msg=error-fetching'));
 
-      // get company details
-      $getCompany = "SELECT * FROM companies WHERE _id = '$company'";
-      $qCompany = mysqli_query($db, $getCompany) or die(header('location: ' . $server . '?msg=error-fetching'));
+      if (mysqli_num_rows($qUser) === 1) {
+        $row = mysqli_fetch_assoc($qUser);
+        $user = $row;
 
-      if (mysqli_num_rows($qUser) === 1 && mysqli_num_rows($qCompany) === 1) {
-        $rowU = mysqli_fetch_assoc($qUser);
-        $user = $rowU;
-
-        $rowC = mysqli_fetch_assoc($qCompany);
-        $userCompany = $rowC;
-
-        /* 
-          success saving 
-          set session as a login user
+        /**
+          * success saving
+          * set session as a login user
         */
 
         session_start();
         $_SESSION['_login'] = true;
         $_SESSION['_user'] = $user;
-        $_SESSION['_company'] = $userCompany;
 
         // redirect with success
-        header('location: ' . $server . '?msg=suclogin');
+        header('location: ../index.php?msg=suclogin');
 
       } else {
         // return to home page
-        header('location: ' . $server . '?msg=error-login');
+        header('location: ../index.php?msg=error-login');
       }
     }
   }
 
 } else {
   // return to home page
-  header('location: ' . $server . '?msg=required');
+  header('location: ../index.php?msg=required');
 }
