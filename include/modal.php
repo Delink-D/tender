cd6f81d606ef7bfad2a993e93526d4e112bf8294
Delink-D <delinkdesigns@gmail.com>
2018-09-02 23:35:56 +0300

    renamed the headings for forms

--- include/modal.php
+++ include/modal.php
@@ -1,9 +1,9 @@
-<!-- Login Modal -->
+<!-- Login Modal Supplier -->
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">SignIn to E-Tender System</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Supplier signIn Form</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
