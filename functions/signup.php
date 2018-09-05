c7bbf5a66a6a77164b078207eaecd673daacd4be
Delink-D <delinkdesigns@gmail.com>
2018-09-06 01:10:46 +0300

    added extra fields to signup form and table

--- functions/signup.php
+++ functions/signup.php
@@ -4,19 +4,21 @@ include '../env/db.php';
 include '../env/server.php';
 
 // gather all the user inputs
-$userName = $_POST['userName'];
+$company = $_POST['cName'];
 $email = $_POST['email'];
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$natId = $_POST['natId'];
+$phone = $_POST['phone'];
+$cat = $_POST['cat'];
+$kra = $_POST['kra'];
+$reg = $_POST['reg'];
+$supid = $_POST['sup_id'];
 $level = $_POST['level'];
 $password = $_POST['password'];
 
-if ($_POST['signup']) {
+if ($_POST['supplier-up']) {
   // if all is good
 
   // check if user exist
-  $check = "SELECT * FROM users WHERE username = '$userName'";
+  $check = "SELECT * FROM suppliers WHERE company_name = '$company'";
   $checkQ = mysqli_query($db, $check) or die(header('location: ../index.php?msg=error-fetching'));
 
   if (mysqli_num_rows($checkQ) > 0) {
@@ -24,14 +26,14 @@ if ($_POST['signup']) {
     exit();
   } else {
     // insert user
-    $sql = "INSERT INTO users (firstname, lastname, username, category, email, nationalId, password)
-          VALUES('$firstName','$lastName','$userName', '$level', '$email','$natId','$password')";
+    $sql = "INSERT INTO suppliers (company_reg, company_pin, company_name, company_phone, company_email, company_cat, supplier_id, level, password)
+          VALUES('$reg','$kra','$company','$phone','$email', '$cat', '$supid','$level','$password')";
     $query = mysqli_query($db, $sql) or die(header('location: ../index.php?msg=error-saving'));
 
     if ($query) {
       // get saved user
       $id = mysqli_insert_id($db);
-      $getUser = "SELECT * FROM users WHERE _id = '$id'";
+      $getUser = "SELECT * FROM suppliers WHERE _id = '$id'";
       $qUser = mysqli_query($db, $getUser) or die(header('location: ../index.php?msg=error-fetching'));
 
       if (mysqli_num_rows($qUser) === 1) {
